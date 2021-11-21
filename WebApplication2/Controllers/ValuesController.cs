ca88653c87ae98db6e00d8d628dee9eb4ee0bf07
Mehmood Shafiq <mehmood.shafiq@shispare.com>
2021-11-21 23:22:35 +0500

    Git-Push

---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Success");
+        }
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }  
+
+
+    }
+}
