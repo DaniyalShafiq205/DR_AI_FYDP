b7c3ff1c1cd58691e99cd78937ed4527983f0ace
Mehmood Shafiq <56289441+DaniyalShafiq205@users.noreply.github.com>
2022-01-04 00:56:16 +0500

    push api work

---
+++ WebApplication2/Controllers/Home1Controller.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Home1Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            
+            LoginModel obj = new LoginModel();
+            return Ok(obj);
+
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(LoginModel objuserlogin)
+        {
+           
+            var display = Userloginvalues().Where(m => m.UserName == objuserlogin.UserName && m.UserPassword == objuserlogin.UserPassword).FirstOrDefault();
+            if (display != null)
+            {
+                /*ViewBag.Status = "CORRECT UserNAme and Password";*/
+                return Ok("correct");
+            }
+            else
+            {
+                /*ViewBag.Status = "INCORRECT UserName or Password";*/
+                return Ok("incorrect");
+            }
+           /* return View(objuserlogin);*/
+        }
+        public List<LoginModel> Userloginvalues()
+        {
+            List<LoginModel> objModel = new List<LoginModel>();
+            objModel.Add(new LoginModel { UserName = "user1", UserPassword = "password1" });
+            objModel.Add(new LoginModel { UserName = "user2", UserPassword = "password2" });
+            objModel.Add(new LoginModel { UserName = "user3", UserPassword = "password3" });
+            objModel.Add(new LoginModel { UserName = "user4", UserPassword = "password4" });
+            objModel.Add(new LoginModel { UserName = "user5", UserPassword = "password5" });
+            return objModel;
+        }
+
+    }
+}
---
+++ WebApplication2/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
+
